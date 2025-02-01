6904669676c7dca714bdbc64fdfde2ccacd1ba5e
Chromoxylograph <chromoxylograph@gmail.com>
2025-01-31 19:53:52 -0600

    Create MenuBackground.java

---
+++ uitest/java/MenuBackground.java
@@ -0,0 +1,11 @@
+package uitest.java;
+
+import mindustry.world.Tiles;
+
+public abstract class MenuBackground {
+    /** Generates a world displayed in the menu. */
+    void generateWorld(int width, int height) {}
+
+    /** Renders the menu background. */
+    void render() {}
+}

c4b7040522a0d8c7dfb5823088640b7fa27a8769
Chromoxylograph <chromoxylograph@gmail.com>
2025-01-31 20:31:19 -0600

    what the ffffsfsf
    fsfsfs

---
+++ gradlewAutoRun.bat
@@ -0,0 +1,43 @@
+set stage=0
+set builded=0
+set copied=0
+@echo off
+
+:START
+cls
+title Gradlew Auto Run w/ Modified gradlew.bat
+echo Gradlew Auto Run w/ Modified gradlew.bat
+echo Created by VvSeanGtvV [ V2 ]
+if %builded%==0 echo ^> :buildJar [ PROGRESS ]
+if %stage%==0 goto buildJar
+if %builded%==1 echo ^> :buildJar [ COMPLETE ]
+if %copied%==0 echo ^> :copyJar  [ PROGRESS ]
+if %stage%==1 goto copyJar
+if %copied%==1 echo ^> :copyJar  [ COMPLETE ]
+if %stage%==2 goto executeMindustry
+
+:buildJar
+echo > :buildJar Started
+title [Gradlew]
+start /wait /b gradlew jar
+set stage=1
+set builded=1
+goto START
+
+:copyJar
+set modsLocation=C:\Users\user\AppData\Roaming\Mindustry\mods
+set buildLocation=D:\user\Documents\GitHub\Project-Restoration\build\libs\Project-RestorationDesktop.jar
+copy %buildLocation% %modsLocation%
+echo set modsLocation=%modsLocation%
+echo set buildLocation=%buildLocation%
+set stage=2
+set copied=1
+goto START
+
+:executeMindustry
+title Mindustry
+set mindustry=D:\user\Downloads\mindustry-windows-64-bit\Mindustry.exe
+echo autorun to %mindustry%
+set stage=3
+%mindustry%
+exit
